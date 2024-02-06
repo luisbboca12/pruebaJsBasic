d61c488f951a63a1ea2db6c78020b30b872b7e33
LuisBarrionuevodev <luisbarrionuevo.dev@gmail.com>
2024-02-06 18:51:05 -0300

    Tipos de datos: Mutabilidad e inmutabilidad

--- prueba.js
+++ prueba.js
@@ -1,3 +1,26 @@
-const perro= 23
+//Tipos de datos: Mutabilidad e inmutabilidad
+//// primitivos-inmutables
+let numero=23
+numero=numero+10
 
-console.log(perro)
+console.log(numero)
+let esVerdadero=true
+esVerdadero=false
+console.log(esVerdadero)
+
+//// complejos Mutables
+
+let usuario={nombre:'Pepito',edad:15}
+usuario.edad=20
+console.log(usuario)
+let frutas=['manzana','pera']
+frutas[0]='sandia'
+console.log(frutas)
+
+
+function cambiarNombre(objeto) {
+    objeto.nombre= "Nuevo Nombre"
+}
+let usuaria= {nombre:"walter",apellido:"diaz"}
+cambiarNombre(usuario)
+console.log(usuario)
