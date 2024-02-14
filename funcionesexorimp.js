cb1daf408c295dbc128b3c7c651442daadf52b3f
LuisBarrionuevodev <luisbarrionuevo.dev@gmail.com>
2024-02-14 19:00:56 -0300

    trabajo2

--- funcionesexorimp.js
+++ funcionesexorimp.js
@@ -8,3 +8,17 @@ const mascota={
     }
 }
 mascota.saludo()
+////enlace explicito
+
+function saludar() {
+    console.log(`Hola ${this.nombre}, tu email es ${this.email} y tus hobbies son ${this.hobbies.join(", ")}.`)
+}
+
+const usuario={
+    nombre:"Luis",
+    email:"luisito.barrionuevodeboca@gmail.com",
+    hobbies:["coding","gym"]
+}
+
+saludar.call(usuario)
+
