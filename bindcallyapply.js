6aec43d9832133fe87ebafe14f311227f9944043
LuisBarrionuevodev <luisbarrionuevo.dev@gmail.com>
2024-02-16 18:55:12 -0300

    viernes

--- bindcallyapply.js
+++ bindcallyapply.js
@@ -19,4 +19,29 @@ llamadoTurno.apply(usuario,turnosDisponibles)
 const llamarTurnoPersona= llamadoTurno.bind(usuario,fechaDisponibles,horaDisponible)
 
 llamarTurnoPersona()
+/////
+////arrow functions and lexi bind(Arrow functions are a way to write functions in a shorter way, but they have some limitations compared to the traditional way. )
+ //traditional function
+ function greetingUser(user){
+    return console.log(`Hi ${user}`)
+ }
 
+ ///arrow function explicit, This way is used when we have two or more parameters
+ const greetingUserWithArrowFunctionExplicit= (user)=>{
+    return console.log(`Hi ${user}`)
+ }
+/// arrow functions implicit 
+const greetingUserWithArrowFunctionImplicit= user=> console.log(`Hi ${user}`)
+
+greetingUser("pedro")
+greetingUserWithArrowFunctionExplicit("pedro")
+greetingUserWithArrowFunctionImplicit("pedro")
+
+const fictionalCharacter=
+{name:'Uncle Ben',
+messageWithTraditionalFunction:function(message){console.log(`${this.name} says: ${message}`)},messageWithArrowFunction:(message)=>{console.log(`${this.name} says: ${message}`)}
+}
+fictionalCharacter.messageWithTraditionalFunction('With great power comes great responsability.')
+fictionalCharacter.messageWithArrowFunction('Beware of Doctor Octopus.')
+
+//Conclusion:When you work with objects and within these objects you create methods, you must create methods with traditional functions when you want the logic (body) of the function to reference some property of the object with the "this" for it to work correctly.
