2894e7c2986cd487c922f6988e79e7c0e2140e4e
luisbboca12 <luisito.barrionuevodeboca@gmail.com>
2024-02-28 18:59:57 -0300

    ss

---
+++ loopfor.js
@@ -0,0 +1,54 @@
+    // const frutas= ["peras","manzanas","bananas","frutillas"]
+
+    // for (let i =0 ; i<frutas.length;i++){
+    //     console.log(frutas[i])
+    // }
+    // const frutas= ["peras","manzanas","bananas","frutillas"]
+
+    //    frutas.forEach(fruta => {
+    //     console.log(fruta)
+        
+    // });
+
+
+    // const numeros = [10,20,30,40,50]
+    // let suma = 0
+
+    // for (const numero of numeros) {
+    //     console.log(suma=numero+10)
+    // }
+
+//     const notasNito=[5,6,6,8,9]
+//     let sum =0
+//    let prom = 0
+    
+//     notasNito.forEach(nota => {
+//          sum +=nota
+//     });
+//  prom= sum / (notasNito.length)
+//     console.log(prom)
+    
+// const notasNito  ={
+//     matematicas : 2,
+//     lengua: 8,
+//     ingles: 5,
+//     gimnasia: 10,
+//     biologia:7
+// } 
+ 
+// for (const nota in notasNito) {
+//     console.log(`${nota} : ${notasNito[nota]} `)
+   
+// }
+
+let contador = 0
+while(contador <=10){
+    console.log(contador)
+    contador ++
+}
+
+do {
+    console.log(contador)
+    co
+
+
