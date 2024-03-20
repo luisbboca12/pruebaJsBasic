ae90fa182bf3e8964d834ddf0cd147719f77d0d0
luisbboca12 <luisito.barrionuevodeboca@gmail.com>
2024-03-20 19:00:23 -0300

    miercoles

--- convertiracelsius.js
+++ convertiracelsius.js
@@ -7,3 +7,9 @@ console.log(convertiraCelsius)
 let acumulador = 0
 let sumarElementos = temperaturaFahrenheit.forEach(temp => acumulador+=temp)
 console.log(acumulador)
+
+let numbers =[1,2,3,4,5,6,7]
+
+let numerosPares = numbers.filter(number=>number %2 ===0)
+
+console.log(numerosPares)
